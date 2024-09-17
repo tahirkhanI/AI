c83e866787dfa02fea88b77adf15fc01272bd356
I Tahir Khan <122418585+tahirkhanI@users.noreply.github.com>
2024-09-17 12:56:44 +0530

    Add files via upload

---
+++ 2.QUEEN.py
@@ -0,0 +1,47 @@
+def is_safe(board, row, col, n):
+    # Check the column for any other queens
+    for i in range(row):
+        if board[i][col] == 1:
+            return False
+    
+    # Check upper-left diagonal
+    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+    
+    # Check upper-right diagonal
+    for i, j in zip(range(row, -1, -1), range(col, n)):
+        if board[i][j] == 1:
+            return False
+
+    return True
+
+def solve_n_queens_util(board, row, n):
+    if row >= n:
+        return True
+
+    for col in range(n):
+        if is_safe(board, row, col, n):
+            board[row][col] = 1  
+
+            if solve_n_queens_util(board, row + 1, n):
+                return True
+
+            
+            board[row][col] = 0
+
+    return False
+
+def solve_n_queens(n):
+    board = [[0 for _ in range(n)] for _ in range(n)]
+
+    if not solve_n_queens_util(board, 0, n):
+        print("Solution does not exist")
+        return False
+
+    for row in board:
+        print(row)
+    return True
+
+n = 4  # For the 4-queen problem
+solve_n_queens(n)
